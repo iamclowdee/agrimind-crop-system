aec8ac52e91ceeaec899d98a142f1444a30321fe
P <159043521+iamclowdee@users.noreply.github.com>
2026-05-11 20:37:24 +0530

    Backend Rollback 01

--- backend/src/routes/cropRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { recommendCrop } = require("../controllers/cropController");
-
-const router = express.Router();
-
-router.post("/recommend", recommendCrop);
-
-module.exports = router;
