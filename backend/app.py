19606c58ca32a73a3285fe2b03058fa9c54800c7
iamclowdee <159043521+iamclowdee@users.noreply.github.com>
2026-06-04 20:22:42 +0530

    app.py update

---
+++ backend/app.py
@@ -0,0 +1,49 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import joblib
+import pandas as pd
+
+app = Flask(__name__)
+CORS(app)
+
+# Load trained model once when server starts
+model = joblib.load("model/crop_model.pkl")
+
+
+@app.route("/")
+def home():
+    return "AgriMind API Running"
+
+
+@app.route("/predict", methods=["POST"])
+def predict():
+
+    try:
+        data = request.json
+
+        features = pd.DataFrame([{
+            "nitrogen": float(data["nitrogen"]),
+            "phosphorus": float(data["phosphorus"]),
+            "potassium": float(data["potassium"]),
+            "temperature": float(data["temperature"]),
+            "humidity": float(data["humidity"]),
+            "ph": float(data["ph"]),
+            "rainfall": float(data["rainfall"])
+        }])
+
+        prediction = model.predict(features)[0]
+
+        return jsonify({
+            "success": True,
+            "recommended_crop": prediction
+        })
+
+    except Exception as e:
+        return jsonify({
+            "success": False,
+            "error": str(e)
+        })
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
